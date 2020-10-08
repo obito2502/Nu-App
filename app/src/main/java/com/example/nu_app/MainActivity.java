deadf16bc633073999b7d5c01fc45f6c18d03e17
yernar_zhalelov <yernarzhalelov@gmail.com>
2020-10-08 18:45:08 +0600

    This is first commit for our senior project

--- app/src/main/java/com/example/nu_app/MainActivity.java
+++ app/src/main/java/com/example/nu_app/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.nu_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sign_in_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sign_in_button = (Button)findViewById(R.id.sign_in);
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+    }
+
+    public void openActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
